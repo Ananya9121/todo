20e207c83ec20fd6145cc69150f7a421bc7b2537
Ananya9121 <104710491+Ananya9121@users.noreply.github.com>
2023-12-07 22:53:00 +0530

    ssl issue-postgresql

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,12 +13,20 @@ import { AuthModule } from './auth/auth.module';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.POSTGRES_HOST,
-      port:parseInt(<string>process.env.POSTGRES_PORT),      
+      port:parseInt(<string>process.env.POSTGRES_PORT,10),      
       username: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DATABASE,
       autoLoadEntities: true,
       synchronize: true,
+      ssl: {
+        rejectUnauthorized: false, // Add this line to accept self-signed certificates
+      },
+      extra: {
+        ssl: {
+          rejectUnauthorized: false, // Add this line to accept self-signed certificates
+        },
+      },
     }),
     UserModule,
     TaskModule,
